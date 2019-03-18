87e30b3b268220fd685df675663060432eeeb293
lfy <lifengying0621@163.com>
2019-03-18 17:18:57 +0800

    个人信息页面

---
+++ app/src/main/java/steam/com/app/Bottom_Bar_Bctivity.java
@@ -0,0 +1,9 @@
+package steam.com.app;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class Bottom_Bar_Bctivity extends AppCompatActivity {
+
+
+
+}
---
+++ app/src/main/java/steam/com/app/P_InformationActivity.java
@@ -0,0 +1,7 @@
+package steam.com.app;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class P_InformationActivity  extends AppCompatActivity {
+
+}
